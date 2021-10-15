aa73a7deb88f1487c0006abcd3b684bf91c2266e
Jiří Staniševský <jirist@salsitasoft.com>
2021-10-15 08:26:43 +0200

    Add sample "code-base"

---
+++ sample/src/index.ts
@@ -0,0 +1,24 @@
+// TODO Single line todo comment
+// TODO(#123) Single line todo comment with reference
+
+/*
+TODO Multiline todo
+ */
+
+
+/*
+TODO(#123) Multiline todo
+ */
+
+// TODO Multiple single
+// TODO line comments
+
+
+// TODO(#123) Multiple single
+// TODO line comments with reference
+
+// TODO: With colon
+
+// TODO: With colon
+
+// TODO: With colon
